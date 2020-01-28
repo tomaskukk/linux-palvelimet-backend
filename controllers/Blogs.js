584a57c5173a4a85bf0e0b54c3e6833ad7493841
TomasKukk <tomsu_kukk@hotmail.com>
2020-01-28 17:09:51 +0200

    first commit

---
+++ controllers/Blogs.js
@@ -0,0 +1,61 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/Blog");
+
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs.map(blog => blog.toJSON()));
+});
+
+blogsRouter.get("/:id", async (request, response, next) => {
+  try {
+    const blog = await Blog.findById(request.params.id);
+    if (blog) {
+      response.json(blog.toJSON());
+    } else {
+      response.status(404).end();
+    }
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+blogsRouter.post("/", async (request, response, next) => {
+  const body = request.body;
+
+  const blog = new Blog({
+    title: body.title,
+    content: body.content,
+    date: new Date()
+  });
+  try {
+    const savedBlog = await blog.save();
+    response.json(savedBlog.toJSON());
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+blogsRouter.delete("/:id", async (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id);
+    response.status(204).end();
+  } catch (exception) {
+    next(exception);
+  }
+});
+
+blogsRouter.put("/:id", (request, response, next) => {
+  const body = request.body;
+
+  const blog = {
+    content: body.content
+  };
+
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON());
+    })
+    .catch(error => next(error));
+});
+
+module.exports = blogsRouter;
---
+++ models/Blog.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  text: String,
+  date: Date,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+});
+
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+module.exports = Blog;
