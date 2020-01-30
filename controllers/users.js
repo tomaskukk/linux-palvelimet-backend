a72ddc2ec39abde681b1475141d899bf77b0d2f2
tomaskukk <tomsu_kukk@hotmail.com>
2020-01-30 18:37:08 +0200

    added blogposts by user

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,15 @@
 const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = request => {
+  const authorization = request.get("Authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
 
 usersRouter.post("/", async (request, response, next) => {
   try {
@@ -30,6 +39,27 @@ usersRouter.get("/", async (request, response) => {
     date: 1
   });
   response.json(users.map(u => u.toJSON()));
+});
+
+usersRouter.get("/blogs", async (request, response) => {
+  const token = getTokenFrom(request);
+  console.log(token);
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    console.log(decodedToken);
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+    const user = await User.findById(decodedToken.id).populate("blogs", {
+      title: 1,
+      content: 1,
+      date: 1
+    });
+    console.log(user);
+    response.json(user.blogs);
+  } catch (exception) {
+    console.log(exception);
+  }
 });
 
 module.exports = usersRouter;
